0955ca70660c2277ed60c65db7f981d8e9c8076f
278882047@qq.com <tjm19960701>
2024-12-25 14:23:44 +0800

    fix.

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -116,7 +116,7 @@ public class SecurityConfig
                     // 静态资源，可匿名访问
                     .antMatchers(HttpMethod.GET, "/", "/*.html", "/**/*.html", "/**/*.css", "/**/*.js", "/profile/**").permitAll()
                     .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
-                    .antMatchers("/business/info", "/business/feedback/add", "/business/product/model/getFilterData", "/business/product/field/getByProductId", "/business/product/model/list", "/business/product/model/search", "/business/file/**").permitAll()
+                    .antMatchers("/business/info", "/business/feedback/add", "/business/product/model/getFilterData", "/business/product/list", "/business/product/field/getByProductId", "/business/product/model/list", "/business/product/model/search", "/business/file/**").permitAll()
                     .antMatchers("/business/information/list", "/business/design/list").permitAll()
                     // 除上面外的所有请求全部需要鉴权认证
                     .anyRequest().authenticated();
